165d6c2e9cf156868d2631c0878e01271d149617
jdgleaver <james@leaver.myzen.co.uk>
2021-03-10 16:56:34 +0000

    Fix paths in Android.mk

--- platform/libretro/jni/Android.mk
+++ platform/libretro/jni/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 DEBUG                    := 0
 FLAGS                    :=
 
-CORE_DIR := $(LOCAL_PATH)/..
+CORE_DIR := $(LOCAL_PATH)/../../..
 
-include $(CORE_DIR)/Makefile.common
+include $(LOCAL_PATH)/../Makefile.common
 
 COREFLAGS := $(INCFLAGS) $(FLAGS)
 COREFLAGS += -D__LIBRETRO__
@@ -20,6 +20,6 @@ LOCAL_MODULE       := retro
 LOCAL_SRC_FILES    := $(SOURCES_C)
 LOCAL_CFLAGS       := $(COREFLAGS)
 LOCAL_CXXFLAGS     := $(COREFLAGS)
-LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
+LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/platform/libretro/link.T
 LOCAL_CPP_FEATURES := exceptions
 include $(BUILD_SHARED_LIBRARY)
